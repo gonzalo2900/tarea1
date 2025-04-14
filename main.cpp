5cd1d0d827d2114bf435d5539e67b05d6f716bd8
gonzalo2900 <chalitomarley10@gmail.com>
2025-04-14 17:33:52 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "clases.h"
-
 //
 // FUNCIÓN PRINCIPAL
 // Crea una instancia del sistema de evaluación y llama al menú principal.
 //
+
 int main() {
     Sistema_Evaluacion sistema;
     sistema.menu();
